1d40a2a3dd38cd84ced838aef54b58f1ca320846
jiyei <134039482+jiyeKa@users.noreply.github.com>
2024-07-30 23:13:05 +0900

    [Silver I] Title: 기타리스트, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,22 @@
+import sys
+
+N,S,M = map(int,sys.stdin.readline().split())
+V = list(map(int,sys.stdin.readline().split()))
+
+dp=[[0]*(M+1) for _ in range(N+1)]
+dp[0][S] =1
+
+for i in range(1,N+1): 
+    for j in range(M+1):
+        if dp[i-1][j]!=0: 
+            if 0<=j+V[i-1]<=M:
+                dp[i][j+V[i-1]]=1
+            if 0<=j-V[i-1]<=M:
+                dp[i][j-V[i-1]]=1
+answer =-1
+for i in range(M, -1, -1):
+    if dp[N][i]==1: 
+        answer = i
+        break
+    
+print(answer)
